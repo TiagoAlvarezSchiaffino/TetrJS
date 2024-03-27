cbcdc325ab752392ddcd9413dc9f57d9b5a9d1b3
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-03-27 14:59:16 -0300

    feat: re-render to implement movement logic

--- tetris-challenge/pages/tetris.js
+++ tetris-challenge/pages/tetris.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 const BOARD_X = 10
 const BOARD_Y = 21
 
@@ -19,13 +21,26 @@ class Tetris {
             shape,
         }
 
+        this.place()
+
+    }
+
+    place({ remove = false } = {}) {
+        const { shape } = this.piece
         for(let y = 0; y < shape.length; y++) {
             for(let x = 0; x < shape[0].length; x++) {
                 const newY = this.piece.y + y
                 const newX = this.piece.x + x
-                this.board[newY][newX] = shape[y][x]
+                this.board[newY][newX] = remove ? 0 : shape[y][x]
             }
         }
+    }
+
+    move({ dx = 0, dy = 0 }) {
+        this.place({ remove: true })
+        this.piece.x += dx
+        this.piece.y += dy
+        this.place()
     }
 }
 
@@ -33,13 +48,29 @@ class Tetris {
 
 const tetris = new Tetris()
 
-const cellStyles = {
+const cellStyles = (cell) => ({
     width: '40px',
     height: '40px',
     border: '1px solid white',
-}
+    backgroundColor: cell === 1 ? 'silver' : 'black'
+})
 
 export default function TetrisGame() {
+    const [_, render] = useState({})
+
+    useEffect(() => {
+        const keyDownHandler = (e) => {
+            console.log('key', e)
+            if (e.key === 'ArrowDown') {
+                tetris.move({ dy: 1 })
+            }
+            render({})
+        }
+        document.addEventListener('keydown', keyDownHandler)
+
+        return () => document.removeEventListener('keydown', keyDownHandler)
+    })
+
     return (
         <>
             <h1>
@@ -48,7 +79,7 @@ export default function TetrisGame() {
             <div>
                 {tetris.board.map((row, i) => (
                 <div key={i} style={{ display: 'flex' }}>
-                    {row.map((cell, j) => <div key={j} style={cellStyles}>{cell}</div>)}
+                    {row.map((cell, j) => <div key={j} style={cellStyles(cell)} />)}
                 </div>
             ))}
             </div>
