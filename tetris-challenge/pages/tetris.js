e69a14077a9cfa865597fa15711f204f696ba392
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-03-27 14:43:13 -0300

    feat: Generation of pieces

--- tetris-challenge/pages/tetris.js
+++ tetris-challenge/pages/tetris.js
@@ -1,11 +1,35 @@
 const BOARD_X = 10
 const BOARD_Y = 21
 
+const SHAPES = [
+    [[1,1,1,1]],
+]
+
 class Tetris {
     constructor() {
         this.board = Array(BOARD_Y).fill("").map(() => Array(BOARD_X).fill(0))
+        this.generatePiece()
+    }
+
+    generatePiece() {
+        const shape = SHAPES[Math.floor(Math.random() * SHAPES.length)]
+        this.piece = {
+            x: BOARD_X/2,
+            y: 0,
+            shape,
+        }
+
+        for(let y = 0; y < shape.length; y++) {
+            for(let x = 0; x < shape[0].length; x++) {
+                const newY = this.piece.y + y
+                const newX = this.piece.x + x
+                this.board[newY][newX] = shape[y][x]
+            }
+        }
     }
 }
+
+
 
 const tetris = new Tetris()
 
