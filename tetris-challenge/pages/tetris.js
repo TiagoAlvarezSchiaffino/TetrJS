e09c9b23aa1822972615e2a30fa72c619f8cc8b1
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-03-27 14:31:48 -0300

    feat: Implement BOARD

--- tetris-challenge/pages/tetris.js
+++ tetris-challenge/pages/tetris.js
@@ -1,9 +1,33 @@
-export default function Tetris() {
+const BOARD_X = 10
+const BOARD_Y = 21
+
+class Tetris {
+    constructor() {
+        this.board = Array(BOARD_Y).fill("").map(() => Array(BOARD_X).fill(0))
+    }
+}
+
+const tetris = new Tetris()
+
+const cellStyles = {
+    width: '40px',
+    height: '40px',
+    border: '1px solid white',
+}
+
+export default function TetrisGame() {
     return (
         <>
             <h1>
                 Tetris
             </h1>
+            <div>
+                {tetris.board.map((row, i) => (
+                <div key={i} style={{ display: 'flex' }}>
+                    {row.map((cell, j) => <div key={j} style={cellStyles}>{cell}</div>)}
+                </div>
+            ))}
+            </div>
         </>
     )
 }
